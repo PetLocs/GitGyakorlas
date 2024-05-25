c07b21cd0743075f0a7c65bb22b86d24bfcf43ea
Yclaze <teralocsi@gmail.com>
2024-05-25 09:10:27 +0200

    void osszegzes()

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -4,6 +4,12 @@ public class GitGyakorlas {
 
     public static void main(String[] args) {
         int[] tomb = new int[] {4, 2, -2, 10, 7};
+        
+        osszegzes();
+    }
+
+    private static void osszegzes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
