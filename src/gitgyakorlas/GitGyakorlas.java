5f68c53112dc7e594547981d3135cfc45630cdca
Yclaze <teralocsi@gmail.com>
2024-05-25 09:18:14 +0200

    tomb, field

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -2,8 +2,8 @@ package gitgyakorlas;
 
 public class GitGyakorlas {
 
+    public static int[] tomb = new int[] {4, 2, -2, 10, 7};
     public static void main(String[] args) {
-        int[] tomb = new int[] {4, 2, -2, 10, 7};
         
         
         kimenet(osszegzes(tomb));
