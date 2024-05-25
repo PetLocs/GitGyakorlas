36e8f1e4af540e01ef9a2f39b10b9b9db42c522b
Yclaze <teralocsi@gmail.com>
2024-05-25 10:16:03 +0200

    formazas, kimenet()

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -4,8 +4,8 @@ public class GitGyakorlas {
 
     public static int[] tomb = new int[] {4, 2, -2, 10, 7};
     public static void main(String[] args) {    
-        kimenet("A tömb elemei: ", "Összege", osszegzes(tomb));
-        kimenet("A tömb elemei: ", "4-nél kisebb számok: ", kisebbMint(4));
+        kimenet("A tömb elemei: [", "Összege", osszegzes(tomb));
+        kimenet("A tömb elemei: [", "4-nél kisebb számok: ", kisebbMint(4));
     }
 
     private static int osszegzes(int [] tomb) {
@@ -21,7 +21,7 @@ public class GitGyakorlas {
         for (int i = 0; i < tomb.length; i++) {
             System.out.print(tomb[i] + " ");
         }
-        System.out.println("\n"+ szoveg2 + szam);
+        System.out.println("]\n"+ szoveg2 + szam);
     }
 
     private static int kisebbMint(int szam) {
