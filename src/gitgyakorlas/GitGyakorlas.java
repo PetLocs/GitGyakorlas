6c8bf58c008bd869b2dc378d34818482e0d17951
Yclaze <teralocsi@gmail.com>
2024-05-25 09:22:59 +0200

    formazas

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -3,9 +3,7 @@ package gitgyakorlas;
 public class GitGyakorlas {
 
     public static int[] tomb = new int[] {4, 2, -2, 10, 7};
-    public static void main(String[] args) {
-        
-        
+    public static void main(String[] args) {    
         kimenet(osszegzes(tomb));
     }
 
