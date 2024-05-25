e09bd61178ad662189aa9f8d7a19d8bf25f48586
Yclaze <teralocsi@gmail.com>
2024-05-25 10:42:21 +0200

    impl void minKivalaszt()

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -47,7 +47,7 @@ public class GitGyakorlas {
     }
 
     private static void minKivalaszt() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
 }
