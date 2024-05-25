973d36cc0a611051e95b3426292317c5c6a915de
Yclaze <teralocsi@gmail.com>
2024-05-25 10:50:03 +0200

    impl void eldontes()

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -59,7 +59,7 @@ public class GitGyakorlas {
     }
 
     private static void eldontes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
 }
